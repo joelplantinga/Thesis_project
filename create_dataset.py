aca71a5b103d9b24596fa5c1750730d8d25447d8
joelplantinga <joelplantinga@hotmail.com>
2021-09-23 14:38:21 +0200

    Finished dataset

--- create_dataset.py
+++ create_dataset.py
@@ -2,29 +2,51 @@ import random
 import pandas as pd
 import numpy as np
 from datetime import datetime
+from collections import namedtuple
 
 class Dataset:
     
-    """Class that makes the dataset.
+    """
+    Class that makes the dataset.
 
-    At the initialization, the environment gets created. Using create_basis()
-    all basis features are randomly generated from the environment. The cost
-    function is added using create_calculations().
+    Attributes:
+    ------------
+
+    x_users : int
+        The number of users that are generated in the environment
+        (default is 4)
+
+    Methods:
+    ------------
+
+    generate_prompts(period=365, min_per_new_prompt=10, date_dep=True)
+        generates a sample dataset of the system ran over a given period.
+
     """
 
     
-    def __init__(self, N):
+    def __init__(self, x_users=100):
         
-        """Defines all scopes that can be taken by the variables."""
+        """
+        Parameters:
+        ------------
 
-        self.N = N
+        x_users : int
+            The number of users that are generated in the environment
+            (default is 4)
+        """
+
+        # Tuple objects that are used throughout the class.
+        self.Prompt = namedtuple("Prompt", 'prompt_type prompt_description prompt_weight')
+        self.Room = namedtuple('Room', 'room_x room_y room_floor')
+        self.User = namedtuple("User", 'id user_x user_y user_floor user_weight team feedback')
+        self.Row = namedtuple('Row', self.User._fields + self.Room._fields + self.Prompt._fields + 
+                              ('tasks_performed_day', 'team_prompts',  'goalsetting', 'date_time', 'total_weight', ))
 
         # User information
-        self.user = [("user_" + str(x+1), random.uniform(0, 1)) for x in range(5)]
-        self.tasks_performed_today = np.arange(0, 7, 1)
+        self.users = self.__create_users(x_users)
 
         # Task information
-        self.prompt_type = ["question", "task"]
         self.prompt_description_task = [
             ("close_window", 0.20), ("open_window", 0.20),
             ("enable_airco", 0.25), ("disable_airco", 0.25),
@@ -37,165 +59,292 @@ class Dataset:
             ("check_airco", 0.10), ("check_heating", 0.10),
             ]
 
-        # General information
-        
-        self.x_coord = np.arange(0, 100, 0.5)
-        self.y_coord = np.arange(0, 40, 0.5)
-        self.floor = np.arange(0, 8, 1)
+        self.prompts = self.__create_prompts()
 
-        # The dataset has a 2 year span
-        self.date = pd.date_range("2021-01-01 00:00:00.000000", periods=2*365).tolist()
-        self.team = ["team_" + str(x+1) for x in range(3)]
+        # General information
+        self.rooms = self.__create_rooms()
+
+        self.tasks_performed_day = 100  * [0]
 
         # Intervention information
-        self.close_to_goal = np.arange(-1, 1, 0.001)
-        self.given_feedback = [True, False]
+        self.team_prompts = 8 * [0, -1]
+        self.team_goals = 16 * [0]
+    
+    def __create_prompts(self):
 
-    def __get_prompt(self, prompt):
+        """
+        Function that creates all prompt objects.
         
-        """Takes the prompt_type,
-        outputs a random prompt from the prompt_type.
+        Returns:
+        ------------
 
-        """
-        if prompt == "task":
-            return random.choice(self.prompt_description_task)
-        else:
-            return random.choice(self.prompt_description_question)
-
-    def __normal_calculation(self, row):
-        """Cost function that is not influenced by the date feature.
-
-        Input is a row within the dataframe,
-        output is the costs associated with it.
+        tasks : list[Prompt]
+            List containing all different prompts
         """
 
-        calc = 1  # initial weight set to 1
+        tasks = []
 
-        calc = calc * ((row.User_weight + 1) / 2)
-
-        dist = pow(pow(row.X_coord_task - row.X_coord_user, 2) +
-                   pow(row.Y_coord_task - row.Y_coord_user, 2), 0.5)
-        calc = calc * (1 - (dist / 108 * 0.25))  # 108 +/- = max diff in dist
-
-        calc = calc * (1 - (abs(row.Floor_user - row.Floor_task) * (1/7) * 0.5))
-
-        calc = calc * (1 - (row.Tasks_performed_today * 0.02))
-
-        calc = calc * (1 - row.Prompt_weight)
-
-        calc = calc * 1  # team does not influence the weight
-
-        return calc
-
-    def __make_date_dependent(self, weight, date):
-        """Calculation of weight function, given that
-        it is dependent on the date of the task being executed.
-
-        A little dependency is created on the passage on time. A bit
-        more dependency is created on the season of the year. In summertime 
-        people are more eager to perform tasks and questions.
-
-        input: current weight, date.
-        output: weight dependent on date.
-        """
-        start_date = datetime.strptime("2021-01-01", "%Y-%m-%d")
-        date_diff = (date - start_date).days
-
-        weight = weight * (1 - (date_diff / 10000))
+        for task in self.prompt_description_task:
+            description, weight = task
+            tasks.append(self.Prompt("task", description, weight))
         
-        season = abs(6 - min(date.month, 12-date.month))
-        weight = weight * (1.09 - (season * 0.03))
+        for question in self.prompt_description_question:
+            description, weight = question
+            tasks.append(self.Prompt("question", description, weight))
+
+        return tasks
+
+    def __create_rooms(self):
+
+        """
+        Creates the rooms of the building. All coordinates are midpoints.
+        
+        Returns:
+        ------------
+
+        rooms : list[Room]
+            List containing all rooms.
+        """
+
+        rooms = []
+        # Room = namedtuple('Room', ['room_x room_y room_floor'])
+        for floor in range(8):
+            for x in range(10, 91, 80):
+                rooms.append(self.Room(x, 20, floor))
+            
+            for x in range(30, 71, 20):
+                for y in range(10, 31, 20):
+                    rooms.append(self.Room(x, y, floor))
+
+        return rooms
+
+    def __create_users(self, x_users):
+        
+        """
+        Creates the users of the organization.
+
+        Attributes:
+        ------------
+
+        x_users : int
+            The number of users that are generated in the environment
+            (default is 4)
+
+        Returns:
+        ------------
+
+        users : list[User]
+            List containing all rooms.
+        """
+
+        
+
+        users = []
+
+        for id, user in enumerate(range(x_users)):
+
+            floor = random.randint(0, 7)
+            x = round(random.uniform(0, 100), 2)
+            team = floor * 2 + (x > 50)
+
+            users.append(self.User(id, x, round(random.uniform(0, 40), 2),
+                                   floor, round(random.uniform(0, 1), 3),
+                                   team, random.randint(0, 1)))
+
+        # print(*users, sep="\n")
+        return users               
+
+    def __calculate_weight(self, row, date_dep):
+
+        """Calculating the changes that a task will be performed using a static
+        formula. 
+
+        Attributes:
+        ------------
+
+        row : Row object
+            A row from the dataset containing all information about the prompt.
+        
+        min_per_new_prompt : int
+            Parameter that indicates the time between the newly generated prompts. 
+        
+        period : int
+            the timespan (in days) over the dataset will be created.
+        
+        date_dep : bool
+            Indicates whether performance indication will be dependent on the date
+            or not.
+
+        Returns:
+        ------------
+
+        weight : float
+            Likelihood that the prompt will be performed by the user.
+
+        """
+
+        weight = 1  # initial weight set to 1
+
+        weight *=  ((row.user_weight + 1) / 2)
+
+        dist = pow(pow(row.room_x - row.user_x, 2) +
+                   pow(row.room_y - row.user_y, 2), 0.5)
+        weight *=  (1 - (dist / 108 * 0.25))  # 108 =~ max diff in dist
+
+        weight *=  (1 - (abs(row.user_floor - row.room_floor) * (1/7) * 0.6))
+
+        weight *=  (1 - (row.tasks_performed_day * 0.02))
+
+        weight *=  (1 - row.prompt_weight)
+
+        weight *=  1  # team does not influence the weight
+
+        
+        # Dependency on the dates
+        if(date_dep):
+            start_date = datetime.strptime("2021-01-01", "%Y-%m-%d")
+            date_diff = (row.date_time - start_date).days
+
+            weight = weight * (1 - (date_diff / 10000))
+            
+            season = abs(6 - min(row.date_time.month, 12-row.date_time.month))
+            weight = weight * (1.09 - (season * 0.03))
+
+        if(row.feedback):
+            weight *= 1.10
+        
+        if(row.team_prompts != -1):
+            # People tend to lose motivation nearing the goal
+            weight *= 1 + (0.1 * (1 - row.goalsetting))
 
         return weight
 
-    def __include_interventions(self, weight, goal, feedback):
+    def __generate_prompts_day(self, time, min_per_new_prompt, date_dep, prompt_goal):
         
-        """"Adds the effects from the intervention techniques 
-        to the weight.
+        """Function that generates prompt for 1 day. Prompts are generated 
+        from 8.00 and stop generating no later than 19.00. The time between
+        newly generated prompts can be modified using the parameter. 
+        
+        Attributes:
+        ------------
 
-        -   Feedback works positively for the succes rate.
-        -   If goalsetting is enabled, people are most motivated far from
-            the goal and lose motivation once the goal approaches.
+        time : Timestamp object
+            Indicates the date and time of the day. Prompts will be generated 11 
+            hours from then.
+        
+        min_per_new_prompt : int
+            Parameter that indicates the time between the newly generated prompts. 
+        
+        period : int
+            The timespan (in days) over the dataset will be created.
+        
+        date_dep : bool
+            Indicates whether performance indication will be dependent on the date
+            or not.
+        
+        prompt_goal : int
+            Indicates the amount of tasks that should be performed in 1 year to pass
+            the goal.
 
+        Returns:
+        ------------
+
+        prompts : pd.Dataframe
+            Dataframe containing prompts of one day. 
+        """
+        prompts = []
+        
+        while (time.hour < 19):
+
+            random_order = random.sample(range(0, 100), 100)
+            room = random.choice(self.rooms)
+            prompt = random.choice(self.prompts)
+
+            user_time = time
+
+            for x in random_order:
+                
+                user = self.users[x]
+                row = self.Row(*user, *room, *prompt, self.tasks_performed_day[user.id],
+                               self.team_prompts[user.team], self.team_goals[user.team], 
+                               user_time, 0)
+
+                weight = self.__calculate_weight(row, date_dep)
+                row = row._replace(total_weight=weight)
+                self.tasks_performed_day[row.id] += 1
+
+                prompts.append(row)
+
+                if (weight > 0.5):
+                    if (row.team_prompts != -1): 
+                        self.team_prompts[user.team] += 1
+                        self.team_goals[user.team] += 1 / prompt_goal
+                        self.team_goals[user.team] = min(self.team_goals[user.team], 1)
+                    break
+                else:
+                    user_time += pd.Timedelta(minutes=3)
+            
+            time += pd.Timedelta(minutes=min_per_new_prompt)
+
+        return pd.DataFrame(prompts)
+        
+    def generate_prompts(self, period=365, min_per_new_prompt=10, date_dep=True):
+        
+        """Function that generates a dataset consisting of prompts that are
+        pushed to users of the system. 
+
+        NOTE: The goalsetting effect is calculated in a such a way that every team could
+        meet the goal if the tasks where equally performed over the teams. 
+
+
+        Attributes:
+        ------------
+        
+        period : int
+            The timespan (in days) over the dataset will be created.
+
+        min_per_new_prompt : int
+            Parameter that indicates the time between the newly generated prompts. 
+                
+        date_dep : bool
+            Indicates whether performance indication will be dependent on the date
+            or not.
+        
+        Returns:
+        ------------
+
+        prompts : pd.Dataframe
+            Dataframe containing prompts over the given period. 
         """
 
-        if(feedback):
-            weight = weight * 1.10
+        prompts = pd.DataFrame()
+        dates = pd.date_range("2021-01-01 08:00:00.000000", periods=period).tolist()
+
+        # Amount of tasks that should be performed to pass the goal.
+        # ((Minutes per day / time between newly generated tasks) * year) / teams
+        prompt_goal = (((11 * 60) / min_per_new_prompt) * 365) / 16
+
+
+        for date in dates:
+                        
+            self.tasks_performed_day = 100  * [0] # Reset task count
+
+            if (date.day == 1 and date.month == 1):
+                self.team_prompts = 8 * [0, -1]
+
+            prompts = prompts.append(self.__generate_prompts_day(date, min_per_new_prompt, date_dep, prompt_goal))
         
-        if(goal > 0):
-            weight = weight * (1 + (0.1 * (1 - goal)))
-
-        return weight
-
-    def print_N(self):
-        print(self.N)
-
-    def create_basis(self):
-        """Creates all basis variables before any calculation in regard to the
-        weight has been performed. Returns the basis dataset.
-
-        """
-        data = pd.DataFrame()
-
-        # User
-        data["User"], data["User_weight"] = zip(*[random.choice(self.user) for i in range(self.N)])
-        data["X_coord_user"] = np.random.choice(self.x_coord, size=self.N)
-        data["Y_coord_user"] = np.random.choice(self.y_coord, size=self.N)
-        data["Floor_user"] = np.random.choice(self.floor, size=self.N)
-        data["Tasks_performed_today"] = np.random.choice(self.tasks_performed_today, size=self.N)
-
-        # Prompt
-        data["X_coord_task"] = np.random.choice(self.x_coord, size=self.N)
-        data["Y_coord_task"] = np.random.choice(self.y_coord, size=self.N)
-        data["Floor_task"] = np.random.choice(self.floor, size=self.N)
-        data["prompt_type"] = np.random.choice(self.prompt_type, size=self.N)
-        data["Prompt_desc"], data["Prompt_weight"] = zip(*[self.__get_prompt(prompt) for prompt in data["prompt_type"]])
-
-        # General
-        data["Date"] = np.random.choice(self.date, size=self.N)
-        data["Team"] = np.random.choice(self.team, size=self.N)
-
-        # Intervention
-        data["Close_to_goal"] = np.random.choice(self.close_to_goal, size=self.N)
-        data["Given_feedback"] = np.random.choice(self.given_feedback, size=self.N)
-
-        return(data)
-
-    def create_calculation(self, df, date_dependent=True):
-        """Calculates the y (costs) column."""
-
-        df["Weight"] = pd.Series(
-
-            self.__normal_calculation(row)
-            for row in df.itertuples()
-        )
-
-        if (date_dependent):
-            df["Date_weight"] = [
-                self.__make_date_dependent(weight, date) 
-                for (weight, date) in zip(df["Weight"], df["Date"])
-                ]
-        df["Int_weight"] = [
-            self.__include_interventions(weight, goal, feedback)
-            for (weight, goal, feedback) in
-            zip(df["Date_weight"], df["Close_to_goal"], df["Given_feedback"])
-            ]
-
-        return df
-
-
+        return(prompts)
 
 
 
 print("----------------------------------------------------")
 
-environment = Dataset(100)
-
-first_df = environment.create_basis()
-
-environment.create_calculation(first_df)
 
 
-print(first_df.head(20))
 
-first_df.to_csv("output.csv")
+environment = Dataset()
+
+data = environment.generate_prompts(period=365, date_dep=False)
+data.to_csv("output.csv")
+print(len(data.index))
